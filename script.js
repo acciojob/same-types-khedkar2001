749ce1f3bbdff5ad97d630da8e496b050957fa93
Saurabh Khedkar <70061591+khedkar2001@users.noreply.github.com>
2025-05-08 19:47:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ function isSameType(value1, value2) {
   return typeof value1 === typeof value2;
 }
 
+// do not change the function above.
 
-// do not change the code below.
-let value1 = prompt("Enter Start of the Range.");
-let value2 = prompt("Enter End Of the Range.");
+let input1 = prompt("Enter Start of the Range.");
+let input2 = prompt("Enter End Of the Range.");
+
+// Convert string inputs to actual types
+let value1 = input1 === "NaN" ? NaN : isNaN(input1) ? input1 : Number(input1);
+let value2 = input2 === "NaN" ? NaN : isNaN(input2) ? input2 : Number(input2);
+
 alert(isSameType(value1, value2));
